eb0f72210279475a5e4cf13006950f3980035d22
Hugo Marques <gugu.sm13@gmail.com>
2025-12-26 15:24:01 +0000

    Mudanças do anuncio e gitignore

--- src/simulation.c
+++ src/simulation.c
@@ -185,12 +185,12 @@ void sim_report_energy( t_simulation* sim )
 
 	emf_get_energy( &sim -> emf, emf_energy );
 	double tot_emf = emf_energy[0];
-	for( i = 0; i < 6; i++ ){
+	for( i = 1; i < 6; i++ ){
 		tot_emf += emf_energy[i];
 	}
 
 	double tot_part = 0;
-	for( i = 0; i < sim -> n_species; i++ ){
+	for( i = 1; i < sim -> n_species; i++ ){
 		part_energy[i] = sim -> species[i].energy;
 		tot_part += part_energy[i];
 	}
