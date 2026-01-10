759ec863321cc4c986272e49141d3456e3b4f622
Ines Matos <inesgamer2108@gmail.com>
2026-01-10 16:50:17 +0000

    new mpi.sh

--- src/mpi.sh
+++ src/mpi.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=zpic-mpi
 #SBATCH --account=f202500010hpcvlabuminhoa
 #SBATCH --partition=normal-arm
-#SBATCH --nodes=1
 #SBATCH --ntasks=2
-#SBATCH --cpus-per-task=1
 #SBATCH --time=00:04:00
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
 
-srun ./zpic
+mpirun -np 2 ./zpic
