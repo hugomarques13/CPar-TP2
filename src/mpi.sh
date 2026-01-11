db99cbd91b96e461a83d605aabbd1e03de2f0eab
Ines Matos <inesgamer2108@gmail.com>
2026-01-11 11:14:20 +0000

    new mpi

--- src/mpi.sh
+++ src/mpi.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 #SBATCH --account=f202500010hpcvlabuminhoa
 #SBATCH --partition=normal-arm
-#SBATCH --nodes=1
 #SBATCH --ntasks=2
-#SBATCH --time=00:30:00
+#SBATCH --time=00:4:00
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-
-# Load MPI module
-ml OpenMPI
-
-# Build
-make clean
-make
 
 # Run
 mpirun -np 2 ./zpic
