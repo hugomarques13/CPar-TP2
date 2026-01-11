d2e6dec83e20e32d03cb5b9f52b6cf33bf6ac868
Ines Matos <inesgamer2108@gmail.com>
2026-01-11 11:49:06 +0000

    coisas

--- src/particles.h
+++ src/particles.h
@@ -193,7 +193,7 @@ void spec_grow_buffer( t_species* spec, const int size );
  * @param emf       EM fields
  * @param current   Current density
  */
-void spec_advance( t_species* spec, t_emf* emf, t_current* current );
+void spec_advance( t_species* spec, t_emf* emf, t_current* current, int rank, int size);
 
 /**
  * @brief Returns the total time spent pushing particles (includes boundaries and moving window)
