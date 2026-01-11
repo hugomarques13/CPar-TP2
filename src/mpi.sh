b2bf264d15347b7a0dc036a1398bcaed59a32f4c
Hugo Marques <gugu.sm13@gmail.com>
2026-01-11 03:41:21 +0000

    mpi.sh

--- src/mpi.sh
+++ src/mpi.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #SBATCH --account=f202500010hpcvlabuminhoa
 #SBATCH --partition=normal-arm
+#SBATCH --nodes=1
 #SBATCH --ntasks=2
-#SBATCH --time=00:04:00
+#SBATCH --time=00:30:00
 
+# Load MPI module
+ml OpenMPI
+
+# Build
+make clean
+make
+
+# Run
 mpirun -np 2 ./zpic
