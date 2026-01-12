05cead05f2ce5f74964996f3be25ff5b3003858a
Hugo Marques <gugu.sm13@gmail.com>
2026-01-12 17:38:40 +0000

    Revert "mpi"
    This reverts commit fe1e3369beb74e0bd398e87bf103f1bef91a9ad2.

--- src/mpi.sh
+++ src/mpi.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 #SBATCH --account=f202500010hpcvlabuminhoa
 #SBATCH --partition=normal-arm
-#SBATCH --ntasks=32
-#SBATCH --time=00:10:00
+#SBATCH --ntasks=14
+#SBATCH --time=00:04:00
 
-OUTPUT_FILE="zpic_scaling_results.txt"
-
-# Clear the file if it already exists
-> "$OUTPUT_FILE"
-
-for n in $(seq 1 20); do
-    echo "===== Running with $n tasks =====" >> "$OUTPUT_FILE"
-    srun --ntasks=$n ./zpic >> "$OUTPUT_FILE"
-    echo "" >> "$OUTPUT_FILE"
-done
+srun ./zpic
